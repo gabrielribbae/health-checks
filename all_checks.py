a9193d7a0b94c192a0dc1148172e40b175af2f66
= <user@example.com>
2022-09-14 12:34:22 -0300

    Allow printing more than one error message

--- all_checks.py
+++ all_checks.py
@@ -24,14 +24,20 @@ def check_root_full():
     return check_disk_full(disk="/", min_gb=2, min_percent=10)
 
 def main():
-    checks=[
+    checks = [
         (check_reboot, "Pending Reboot"),
         (check_root_full, "Root partition full"),
     ]
+    everything_ok = True
     for check, msg in checks:
         if check():
             print(msg)
-            sys.exit(1)
+            everything_ok = False
+
+
+    if not everything_ok:
+        sys.exit(1)
+
 
     print("Everything ok.")
     sys.exit(0)
